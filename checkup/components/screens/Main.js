e4471ce2afeabe84ad86562abb56775feb80caf3
Neel Kattumadam <neelkk@gmail.com>
2020-06-20 02:52:34 -0700

    Dynamic search bar with updating Map+ all functionality good on Android+iOS

--- checkup/components/screens/Main.js
+++ checkup/components/screens/Main.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Text, View, TouchableOpacity, Button, StyleSheet} from 'react-native';
 import GetLocation from 'react-native-get-location';
 import auth from '@react-native-firebase/auth';
 import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import {GooglePlacesAutocomplete} from 'react-native-google-places-autocomplete';
 //Maps API Key:AIzaSyAtBj_iX2WW2bYNKg1iyB5b1fEphwYrjzw
 const Main = (props) => {
+  const [location, setLocation] = useState({
+    longitude: -122.023,
+    latitude: 37.2638,
+    latitudeDelta: 0,
+    longitudeDelta: 0,
+  });
   const logout = () => {
     console.log('hello');
     auth()
@@ -30,18 +37,59 @@ const Main = (props) => {
     <View style={styles.container}>
       <Text>Main Screen</Text>
 
+      <View style={styles.searchBar}>
+        <GooglePlacesAutocomplete
+          query={{
+            key: 'AIzaSyDlQf8TSET_y620Z1CYltuN0wPDdXTophg',
+
+            language: 'en', // language of the results
+          }}
+          placeholder="Enter Location"
+          GooglePlacesDetailsQuery={{fields: 'geometry'}}
+          onPress={(data, details = null) =>
+            setLocation({
+              longitude: details.geometry.location.lng,
+              latitude: details.geometry.location.lat,
+              longitudeDelta: 0.0421,
+              latitudeDelta: 0.0922,
+            })
+          }
+          onFail={(error) => console.error(error)}
+          fetchDetails={true}
+          styles={{
+            textInputContainer: {
+              backgroundColor: 'rgba(0,0,0,0)',
+              borderTopWidth: 0,
+              borderBottomWidth: 0,
+            },
+            textInput: {
+              marginLeft: 0,
+              marginRight: 0,
+              height: 38,
+              color: '#5d5d5d',
+              fontSize: 16,
+            },
+            predefinedPlacesDescription: {
+              color: '#1faadb',
+            },
+          }}
+        />
+      </View>
+
       <MapView
         provider={PROVIDER_GOOGLE}
         style={styles.map}
-        initialRegion={{
-          latitude: 37.2638,
-          longitude: -122.023,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
+        initialRegion={location}
+        region={location}
       />
+
       <TouchableOpacity>
-        <Button title={'Logout'} color="orange" onPress={logout} />
+        <Button
+          title={'Logout'}
+          color="orange"
+          onPress={logout}
+          style={styles.logoutButton}
+        />
       </TouchableOpacity>
     </View>
   );
@@ -49,15 +97,14 @@ const Main = (props) => {
 
 const styles = StyleSheet.create({
   container: {
-    ...StyleSheet.absoluteFillObject,
-    height: 400,
-    width: 400,
-    justifyContent: 'flex-end',
-    alignItems: 'center',
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#ecf0f1',
   },
-  map: {
-    ...StyleSheet.absoluteFillObject,
-  },
+  searchBar: {flex: 1},
+  map: {flex: 1},
+
+  logoutButton: {},
 });
 
 export default Main;
